c19daac01d0e49add279619c2cf4183101450d67
eamendez85 <eamendez85@misena.edu.co>
2021-10-12 16:13:55 -0500

    Ejercicios 1 y 2

---
+++ notaMinima/app.js
@@ -0,0 +1,23 @@
+const nota1=document.getElementById("n1");
+const nota2=document.getElementById("n2");
+const resultado=document.getElementById("resultado");
+
+const calculo=document.querySelector('#calcular');
+
+calculo.addEventListener("click", calcular);
+
+function calcular(){
+    let n1=parseFloat(nota1.value) * 0.25;
+    let n2=parseFloat(nota2.value)* 0.35;
+
+    let suma= n1 + n2;
+    let resta=3.5-suma;
+    let op=resta/0.4;
+
+    if (op>5) {
+        resultado.textContent=`Su promedio es ${suma }. No alcanza a pasar`;
+    }
+    else{
+        resultado.textContent=`Su promedio es ${suma }. La nota minima que necesita es: ${op}`;
+    }
+}
